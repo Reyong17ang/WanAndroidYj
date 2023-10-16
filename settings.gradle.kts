6382efa6ab26323a324abfe1b0b4092deedbe6de
yj <563724977@qq.com>
2023-10-16 11:15:41 +0800

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "WanAndroidYj"
+include(":app")
+include(":lib_common")
+include(":lib_framework")
+include(":lib_network")
